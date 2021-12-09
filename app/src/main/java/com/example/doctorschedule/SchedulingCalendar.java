605b28111b8e9e39d5ca772d855b1782136140f1
Jhoseph <jhoseph.jps@gmail.com>
2021-12-09 00:30:29 -0300

    app quase pronto

---
+++ app/src/main/java/com/example/doctorschedule/SchedulingCalendar.java
@@ -0,0 +1,14 @@
+package com.example.doctorschedule;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SchedulingCalendar extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scheduling_calendar);
+    }
+}
